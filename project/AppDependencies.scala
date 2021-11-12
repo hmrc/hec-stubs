a4c1a3ad69626d14f9a9a9f8b900e69c6474a873
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2021-11-12 09:46:25 +0000

    Hec 1398 lib upgrade (#18)
    * HEC-1398 lib upgrade
    
    * HEC-1398 lib upgrade
    
    * HEC-1398 lib dependency change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import play.core.PlayVersion
 import sbt._
 
 object AppDependencies {
-
-  val compile = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % "5.14.0",
+  val bootStrapVersion = "5.16.0"
+  val compile          = Seq(
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % bootStrapVersion,
     "org.typelevel" %% "cats-core"                 % "2.6.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.14.0"            % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootStrapVersion    % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.5"             % Test,
     "org.jsoup"               % "jsoup"                  % "1.13.1"            % Test,
     "com.typesafe.play"      %% "play-test"              % PlayVersion.current % Test,
