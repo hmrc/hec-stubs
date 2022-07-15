7a7473f203126959049c8c40c720505de1a0b315
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2022-07-15 14:08:34 +0100

    HEC-2286: library upgrade (#25)
    * HEC-2286: library upgrade
    
    * update application-json-ogger.xml

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "5.21.0"
+  val bootStrapVersion = "6.3.0"
 
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % bootStrapVersion,
-    "org.typelevel" %% "cats-core"                 % "2.7.0"
+    "org.typelevel" %% "cats-core"                 % "2.8.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootStrapVersion    % Test,
-    "org.scalatest"          %% "scalatest"              % "3.2.10"            % Test,
-    "org.jsoup"               % "jsoup"                  % "1.14.3"            % Test,
+    "org.scalatest"          %% "scalatest"              % "3.2.12"            % Test,
+    "org.jsoup"               % "jsoup"                  % "1.15.2"            % Test,
     "com.typesafe.play"      %% "play-test"              % PlayVersion.current % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.62.2"            % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"             % "test, it"
